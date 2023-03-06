56c34995a375ee553a91a553780d439fae77ae6a
LeeLoo3434 <lolarussell347@gmail.com>
2023-03-06 12:41:45 -0800

    auth completed

---
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,64 @@
+import {useState} from 'react'
+import { signUp } from '../../utilities/users-service'
+
+
+export default function SignUpForm(props) {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirm, setConfirm] = useState('');
+    const [error, setError] = useState('');
+
+const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setError('');
+    switch (name) {
+        case 'name':
+            setName(value);
+            break;
+        case 'email':
+            setEmail(value);
+            break;
+        case 'password':
+            setPassword(value);
+            break;
+        case 'confirm':
+            setConfirm(value);
+            break;
+        default:
+            break;
+    }
+};
+
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+    try {
+        const formData = { name, email, password };
+        const user = await signUp(formData);
+        props.setUser(user);
+    } catch {
+        setError('Sign Up Failed - Try Again');
+    }
+};
+
+const disable = password !== confirm;
+
+    return (
+        <div>
+        <div className="form-container">
+            <form autoComplete="off" onSubmit={handleSubmit}>
+                <label>Name</label>
+                <input type="text" name="name" value={name} onChange={handleChange} required />
+                <label>Email</label>
+                <input type="email" name="email" value={email} onChange={handleChange} required />
+                <label>Password</label>
+                <input type="password" name="password" value={password} onChange={handleChange} required />
+                <label>Confirm</label>
+                <input type="password" name="confirm" value={confirm} onChange={handleChange} required />
+                <button type="submit" disabled={disable}>SIGN UP</button>
+                </form>
+        </div>
+        <p className="error-message">&nbsp;{error}</p>
+    </div>
+);
+}
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,29 @@
 import './App.css';
+import { useState } from 'react';
+import { getUser } from '../../utilities/users-service'
+import { Routes, Route } from 'react-router-dom'
+import AuthPage from '../AuthPage/AuthPage'
+import DashboardPage from '../DashboardPage/DashboardPage'
+import EventsPage from '../EventsPage/EventsPage';
+import NavBar from '../../components/Navbar/Navbar';
 
 export default function App() {
+  const [ user, setUser ] = useState(getUser())
+
   return (
     <main className="App">
-      App
+      {
+        user ?
+        <>
+          <NavBar user={user} setUser={setUser} />
+          <Routes>
+            <Route path="/dashboard/search" element={<DashboardPage />} />
+            <Route path="/events" element={<EventsPage />} />
+          </Routes>
+        </>
+        :
+        <AuthPage setUser={setUser} />
+      }
     </main>
   );
 }

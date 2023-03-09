2415b3e226bac55566b864dbe09ac7e28ae1e5d4
LeeLoo3434 <lolarussell347@gmail.com>
2023-03-09 11:07:55 -0800

    added eventform eventlist and events page

---
+++ src/pages/EventPage/EventPage.jsx
@@ -0,0 +1,10 @@
+import EventForm from "../../components/EventForm/EventForm"
+import EventList from "../../components/EventList/EventList"
+export default function EventPage(){
+    return(
+        <>
+        < EventForm />
+        < EventList />
+        </>
+    )
+}
